703d0fc3a7999a6c479f3931174d32bfb565eaac
FahimReza <FahimReza202@outlook.com>
2025-06-16 15:33:10 +0330

    Create The admin For Cart App

--- core/cart/admin.py
+++ core/cart/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import CartModel, CartItemModel
 # Register your models here.
+
+@admin.register(CartModel)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display=["user", "created_date"]
+    ordering = ["created_date"]
+    search_fields= ["userــemail"]
+
+
+@admin.register(CartItemModel)
+class CartItemModelAdmin(admin.ModelAdmin):
+    list_display=["product__name", "product__price", "quantity", "created_date"]
+    ordering = ["created_date"]
+    search_fields= ["product__price"]  
