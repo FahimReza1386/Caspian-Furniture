a82803b96e8439378848037aa1d779aa5d3a3a1e
FahimReza <FahimReza202@outlook.com>
2025-06-19 12:30:19 +0330

    Create The Payment App

--- core/core/urls.py
+++ core/core/urls.py
@@ -45,6 +45,7 @@ urlpatterns = [
     path("shop/", include("shop.urls")),
     path("cart/", include("cart.urls")),
     path("order/", include("order.urls")),
+    path("payment/", include("payment.urls")),
 
 
     path("swagger/",schema_view.with_ui("swagger", cache_timeout=0), name="shema-swagger-ui"),
